4efe6ec04fedc0dbc2ed1af699700b83af65798c
lolo1504 <manulag46@gmail.com>
2024-04-02 17:45:36 +0200

    Segundo Commit

---
+++ gestion-persona/database/migrations/2024_04_02_152208_create_persona_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('personas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->integer('DNi');
+            $table->string('pais');
+            $table->string('ciudad');  
+            $table->string('calle');
+            $table->string('numero');
+            $table->boolean('soltero');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('personas');
+    }
+};
--- gestion-persona/routes/web.php
+++ gestion-persona/routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\PersonaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['prefix' => 'api'],function(){
+    
+    Route::resource('persona',PersonaController::class);
+
+
+
+
 });
